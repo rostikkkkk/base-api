fcd51516a84808ef9e66afae5c9504721a00a477
Rostislav Martinyuk <kievstarpro01082017@gmail.com>
2024-01-23 21:40:28 +0200

    posts crud

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,44 +1,56 @@
 import {
   Body,
-  Delete,
-  Get,
   Injectable,
+  NotFoundException,
   Param,
-  Patch,
-  Post,
   Query,
 } from '@nestjs/common';
 import { GetPostsDto } from './dto/get-posts.dto';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Post as Posts } from './post.entity';
+
+import { Post as PostEntity } from './post.entity';
 import { PostRepository } from './post.repository';
 
 @Injectable()
 export class PostsService {
-  constructor(private postsRepository: PostRepository) {}
-  @Get()
+  constructor(private readonly postRepository: PostRepository) {}
   getAllPosts(@Query() getPostsDto: GetPostsDto) {
-    return this.postsRepository.find();
+    return this.postRepository.find();
   }
-  @Get(':id')
-  getPostById(@Param('id') id: string) {
-    return `Post by id:${id} `;
-  }
-
-  @Post()
-  createPost(@Body() createPostDto: CreatePostDto) {
-    return 'CReate post';
-  }
-  @Patch(':id')
-  updatePost(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return 'Update';
+  async getPostById(@Param('id') id: string): Promise<PostEntity[]> {
+    const post = await this.postRepository.findBy({
+      id,
+    });
+    if (!post.length) {
+      throw new NotFoundException(`Post not found!`);
+    }
+    return post;
   }
 
-  @Delete(':id')
-  deletePost(@Param('id') id: string) {
-    return 'Delete post';
+  createPost(@Body() createPostDto: CreatePostDto): Promise<PostEntity> {
+    const newPost = this.postRepository.create(createPostDto);
+    return this.postRepository.save(newPost);
+  }
+  async updatePost(
+    @Param('id') id: string,
+    @Body() updatePostDto: UpdatePostDto,
+  ): Promise<PostEntity> {
+    const oldPost = await this.getPostById(id);
+
+    if (!oldPost.length) {
+      throw new NotFoundException(`Post with id:${id} not found!`);
+    }
+
+    const editedPost = { ...oldPost[0], ...updatePostDto };
+    return await this.postRepository.save(editedPost);
+  }
+
+  async deletePost(@Param('id') id: string): Promise<void> {
+    const result = await this.postRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Post with id: ${id} not exist!`);
+    }
   }
 }
